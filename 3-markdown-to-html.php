db2bbb7c87a2c11d60e651b1cb948bc2f2a94b31
DavidUnzue <davidunzue@gmail.com>
2015-01-30 18:15:56 +0100

    first functional version

---
+++ 3-markdown-to-html.php
@@ -0,0 +1,30 @@
+<?php
+include_once "src/markdown.php";
+
+// If form was sent, process input
+if (isset($_POST["submit"])) {
+	try {
+    	$html_output = Markdown(file_get_contents('changes.md'));
+	} catch (Exception $e) {
+	    echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+}
+
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>3- Markdown to HTML</title>
+<link type="text/css" rel="stylesheet" href="src/main.css">
+</head>
+<body>
+	<form method="post">
+		<input type="submit" name="submit" value="Get HTML">
+	</form>
+	<br /><br />
+	<h2>Output</h2>
+	<?php echo isset($html_output) ? $html_output : ''; ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>HTML/Markdown Diff Tool</title>
+<link type="text/css" rel="stylesheet" href="src/main.css">
+</head>
+<body>
+<ol>
+	<li><a href="1-html-to-markdown.php">Transform HTML to Markdown</a></li>
+	<li><a href="2-diff-markdown.php">Diff obtained Markdown</a></li>
+	<li><a href="3-markdown-to-html.php">Transform Markdown back to HTML</a></li>
+</ol>
+</body>
+</html>
