24b1d436ce10fa6427be8ca0e04dd3f702b059eb
DavidUnzue <davidunzue@gmail.com>
2015-02-06 17:49:52 +0100

    Completed php class to provide full functionality. Tests completed and passed

--- src/HtmlDiff.php
+++ src/HtmlDiff.php
@@ -10,14 +10,60 @@ require 'vendor/autoload.php';
 
 class HtmlDiff {
 
+	private $root = ''; // Root directory of class file
+
+	public function __construct()
+	{
+		$this->root = dirname(__FILE__);
+	}
+
 	public function htmlToMarkdown($inputHtml)
 	{
 		$converter = new \Markdownify\Converter;
 	
 		// Convert html to markdown
-		$outputMd = $converter->parseString($inputHtml);
+		$outputMarkdown = $converter->parseString($inputHtml);
 
-		return $outputMd;
+		return $outputMarkdown;
 	}
 
+	public function createFile($filePath, $fileContent)
+	{	
+		try {
+
+			if ( !($handle = fopen($filePath, "w")) ) {
+				 throw new Exception("Unable to open file!");
+			}
+
+		} catch (Exception $e) {
+ 
+		    echo "Message: " . $e->getMessage() . "<br />";
+		    echo "File: " . $e->getFile() . "<br />";
+		    echo "Line: " . $e->getLine();
+
+		}
+
+		fwrite($handle, $fileContent);
+		fclose($handle);
+
+	}
+
+	public function diffMarkdown($oldFilePath, $newFilePath, $diffFilePath)
+	{
+		try {
+    		shell_exec('wdiff ' . $oldFilePath . ' ' . $newFilePath . ' | ' . $this->root . '/markdown-format-wdiff >' . $diffFilePath);
+		} catch (Exception $e) {
+		    echo 'Caught exception: ',  $e->getMessage(), "\n";
+		}
+	}
+
+	public function markdownToHtml($inputMarkdown)
+	{
+		$converter = new \Michelf\Markdown;
+
+		// Convert markdown to html
+		$outputHtml = $converter->transform($inputMarkdown);
+
+		return $outputHtml;
+	}
 }
--- tests/HtmlDiffTest.php
+++ tests/HtmlDiffTest.php
@@ -4,23 +4,107 @@
 require 'vendor/autoload.php';
 
 use HtmlDiff\HtmlDiff;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
+use org\bovigo\vfs\vfsStreamFile;
+use org\bovigo\vfs\vfsStreamDirectory;
 
 class HtmlDiffTest extends PHPUnit_Framework_TestCase {
 
+	// on test start
 	public function setUp()
 	{
+		// create instance of htmlDiff
 		$this->htmlDiff = new HtmlDiff;
+
+		// delete markdown files if they exist in order to get true results of tests
+		foreach (['old.md', 'new.md', 'changes.md'] as $file) {
+			if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $file) === true) {
+            	unlink(dirname(__FILE__) . DIRECTORY_SEPARATOR . $file);
+        	}
+		}
 	}
 
-	public function testItShouldConvertAnEmptyString()
+	// on test end
+	protected function tearDown()
+    {	
+    	// delete markdown files if they exist in order to get true results of tests
+    	foreach (['old.md', 'new.md', 'changes.md'] as $file) {
+			if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $file) === true) {
+            	unlink(dirname(__FILE__) . DIRECTORY_SEPARATOR . $file);
+        	}
+		}
+    }
+
+	public function testItShouldConvertEmptyHtmlToMarkdown()
 	{
 		$this->assertEquals('', $this->htmlDiff->htmlToMarkdown(''));
 	}
 
-	public function testItShouldConvertValidHtml()
+	public function testItShouldConvertValidHtmlToMarkdown()
 	{
 		$inputHtml = '<strong>Text</strong>';
-		$this->assertEquals('**Text**', $this->htmlDiff->htmlToMarkdown($inputHtml));
+		$outputMarkdown = '**Text**';
+		$this->assertEquals($outputMarkdown, $this->htmlDiff->htmlToMarkdown($inputHtml));
+	}
+
+	public function testItShouldCreateAMarkdownFile()
+	{	
+		vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('root'));
+
+        $fileName = 'markdown.md';
+		$filePath = vfsStream::url('root' . DIRECTORY_SEPARATOR . $fileName);
+		$fileContent = '**Text**';
+
+		$this->htmlDiff->createFile($filePath, $fileContent);
+
+		$this->assertTrue(file_exists($filePath));
+		$this->assertEquals($fileContent, file_get_contents($filePath));
+		$this->assertTrue(is_writable($filePath));
+
+	}
+
+	public function testItShouldCreateADiffFile()
+	{
+		vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('root'));
+
+        $oldFilePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'old.md';
+        $oldFileContent = 'Old Text';
+        $this->htmlDiff->createFile($oldFilePath, $oldFileContent);
+
+        $newFilePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'new.md';
+        $newFileContent = 'New Text';
+        $this->htmlDiff->createFile($newFilePath, $newFileContent);
+
+		$diffFilePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'changes.md';
+		$diffFileContent = '<del class="del">Old</del><ins class="ins">New</ins> Text';
+		$diffFileContent .= "\n\n";
+		$diffFileContent .= '<style>';
+		$diffFileContent .= "\n";
+		$diffFileContent .= '    .del,.ins{ display: inline-block; margin-left: 0.5ex; }';
+		$diffFileContent .= "\n";
+		$diffFileContent .= '    .del     { background-color: #fcc; }';
+		$diffFileContent .= "\n";
+		$diffFileContent .= '         .ins{ background-color: #cfc; }';
+		$diffFileContent .= "\n";
+		$diffFileContent .= '</style>';
+		$diffFileContent .= "\n\n";
+
+		$this->htmlDiff->diffMarkdown($oldFilePath, $newFilePath, $diffFilePath);
+		$this->assertTrue(file_exists($diffFilePath));
+		$this->assertEquals($diffFileContent, file_get_contents($diffFilePath));
+		$this->assertTrue(is_writable($diffFilePath));
+
+	}
+
+	public function testItShouldConvertMarkdownToHtml()
+	{
+		$inputMarkdown = '**Text**';
+		$outputHtml = '<p><strong>Text</strong></p>';
+		$outputHtml .= "\n";
+		$this->assertEquals($outputHtml, $this->htmlDiff->markdownToHtml($inputMarkdown));
 	}
 
 }
